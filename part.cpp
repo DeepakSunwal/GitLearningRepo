648839e3f98ecb3ae2982c2d8d2d40b14b5ba402
DeepakSunwal <sunwaldeepak3197@gmail.com>
2021-05-29 15:14:16 +0530

    new change in def

--- part.cpp
+++ part.cpp
@@ -6,5 +6,6 @@ int main()
 	cout<<"This is a c++ program\n";
 	cout<<"and we loved that types of program \n";
 	cout<<"Now we are in the def branch\n";
+	cout<<"Hello Friends these are the new changes\n";
 	return 0;
 }
